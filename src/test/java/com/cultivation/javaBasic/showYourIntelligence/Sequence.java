05a4fcc75fbdcf2d4b088eba63a4b79ad6334519
491818175@qq.com <491818175@qq.com>
2020-08-08 17:15:20 +0800

    [xiawenhao] feat: should create a sequence without putting all items into memory

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/Sequence.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/Sequence.java
@@ -9,7 +9,9 @@ public class Sequence implements Iterable<Integer> {
     private final Integer end;
 
     public Sequence(Integer start, Integer end) {
-        if (start >= end) { throw new IllegalArgumentException("Start must be smaller than End."); }
+        if (start >= end) {
+            throw new IllegalArgumentException("Start must be smaller than End.");
+        }
         this.start = start;
         this.end = end;
     }
@@ -23,13 +25,17 @@ public class Sequence implements Iterable<Integer> {
 class SequenceIterator implements Iterator<Integer> {
     // TODO: You can add additional fields or methods if you want.
     // <--start
-
+    Integer start;
+    Integer end;
+    Integer now;
     // --end-->
 
     SequenceIterator(Integer start, Integer end) {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        this.start = start;
+        this.end = end;
+        this.now = start;
         // --end-->
     }
 
@@ -37,7 +43,7 @@ class SequenceIterator implements Iterator<Integer> {
     public boolean hasNext() {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        return now < end;
         // --end-->
     }
 
@@ -45,7 +51,7 @@ class SequenceIterator implements Iterator<Integer> {
     public Integer next() {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        return now++;
         // --end-->
     }
 }
